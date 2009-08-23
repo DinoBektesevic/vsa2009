d780fa83a112ab2439cd2026f4e93c52119c7462
neven.golenic <neven.golenic@e1cc0a58-8fe3-11de-800e-5b313e0d2bf3>
2009-08-23 14:00:48 +0000

    added mire.py

---
+++ trunk/mire.py
@@ -0,0 +1,9 @@
+import os
+import shutil
+
+fullpath='C:\Documents and Settings\Neven Golenic\Desktop\vsa2009\VICTORIA\mire'
+
+from os.path import join,getsize
+from root, files, dirs in os.walk(fullpath)
+    for images in files:
+        
